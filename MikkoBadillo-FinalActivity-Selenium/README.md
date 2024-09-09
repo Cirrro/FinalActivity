2583fa3576f53f7f054a3e5ebee5ced0838e057a
Cirrro <mikkobadillo18@gmail.com>
2024-09-09 14:33:21 +0800

    Add files via upload

---
+++ MikkoBadillo-FinalActivity-Selenium/README.md
@@ -0,0 +1,1 @@
+# MikkoBadillo-FinalActivity-Selenium
---
+++ MikkoBadillo-FinalActivity-Selenium/src/main/java/testBase/TestBase.java
@@ -0,0 +1,33 @@
+package testBase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	
+	private static final boolean True = false;
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+
+	    @BeforeMethod(alwaysRun=True)
+	    public void setUp() {
+	        WebDriverManager.chromedriver().setup();
+	        driver = new ChromeDriver();
+	        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	        driver.manage().window().maximize();
+	    }
+
+	    @AfterMethod(alwaysRun=True)
+	    public void tearDown() {
+	        if (driver != null) {
+	            driver.quit();
+        }
+    }
+}
